c27a73eef5582052e0ca895111d90edf4c4bdb9a
Jit Debnath <jit.nathdeb@gmail.com>
2025-10-07 20:51:01 +0530

    afefae

--- video-capture_test.py
+++
@@ -1,7 +0,0 @@
-from utility.loader import cnn_loader
-from utility.camera import cv2ModelRunner
-
-
-model, transformer, class_names, device = cnn_loader("models/model.pth","/home/jit/.cache/kagglehub/datasets/msambare/fer2013/versions/1/train")
-
-cv2ModelRunner(model, class_names, transformer, device)
